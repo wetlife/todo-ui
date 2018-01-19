29ca3ce472f754bddda239d75ec13f4693bc4a30
kyle thomas <wetlife@gmail.com>
2018-01-18 20:00:11 -0800

    pocWIP: fix package.json and main.js and npm install then npm star

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 'use strict';
 
+import axios;
+testRequest = function() {
+  axios.get('google.com')
+	.then(function(response) {
+		var div =  document.createElement('div');
+		var textNode = document.createTextNode('foo');
+		div.appendChild(textNode);
+		document.body.appendChild(div);
+	};
+};
+
+//response = axios.get('/
 //code goes here
